0383b779572541ae6f7c3e22b0d75af8107e9e9e
Abdulaziz Alhaqbani <alhaqbani21@gmail.com>
2024-05-26 19:21:28 +0300

    finish lab1

--- script.js
+++ script.js
@@ -8,11 +8,15 @@
 
 // ptag.insertAdjacentHTML('afterend', '<h1>hello</h1>');
 
+// Q1
+
 let headerTag = document.getElementsByClassName('header')[0];
 console.dir(headerTag);
 
 headerTag.style.color = 'purple';
 headerTag.style.backgroundColor = 'blue';
+
+// Q2
 
 let paragraph = document.getElementById('paragraph');
 
@@ -20,10 +24,13 @@ paragraph.style.fontWeight = 'bold';
 
 paragraph.setAttribute('class', 'paragraphStyle');
 
+// Q3
+
 let imgTag = document.getElementsByTagName('img');
 imgTag[0].src =
   'http://t2.gstatic.com/licensed-image?q=tbn:ANd9GcTK3VxkYi7fmb5YQyMqIDiGvuG1angXfgUhabet1v3tT2MDPsm3ho8FppZkdmZPCnMN1Bgdohi1b6sFi6WT7Vw';
 
+//   Q4
 let ulTag = document.querySelector('ul');
 console.log(ulTag);
 
@@ -31,6 +38,7 @@ ulTag.innerHTML =
   '<li class= listStyle>Hello</li><li class= listStyle> All </li><li class= listStyle> of you</li>';
 console.log(document.body);
 
+// Q5
 document.body.innerHTML += `<table class="tableStyle"></table>`;
 
 let tableTag = document.querySelector('table');
